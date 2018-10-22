57566211a56363f0d981e42dba44ad5bd9d72b27
TheBlueSpirit42 <joshua.stein.work@gmail.com>
2018-10-22 10:05:30 -0700

    update

--- input_actual.rs
+++ input_actual.rs
@@ -1,10 +1,40 @@
 use std::io;
 //use std::io::prelude::*;
+fn add(A: i32, B: i32)
+{
+    return (A + B);
+}
 
-fn main() {
-  let mut a_str = String::new();
+fn sub(A: i32, B: i32)
+{
+    return (A - B);
+}
+
+fn mult(A: i32, B: i32)
+{
+    return (A*B);
+}
+
+fn mult(A: i32, B: i32)
+{
+    return (A/B);
+}
+
+fn input()
+{
+    let mut A = String::new();
     io::stdin().read_line(&mut a_str).expect("read error");
-    let mut vec = a_str.split_whitespace()
+    let mut vec = A.split_whitespace()
     .map(|x| x.parse::<i32>().expect("parse error"))
     .collect::<Vec<i32>>();
+    return A;
+}
+
+fn main() {
+    let mut A = input();
+
+    let mut B = input();
+
+    let c = sub(A,B);
+    println!("{}",c);
 }
